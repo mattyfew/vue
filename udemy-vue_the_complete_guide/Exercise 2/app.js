1560416fe5e216303f29c5ffabcc69482f4aee93
Matt Fewer <mattfewer@hotmail.com>
2018-01-13 15:59:11 +0100

    exercise 2 done

--- udemy-vue_the_complete_guide/Exercise 2/app.js
+++ udemy-vue_the_complete_guide/Exercise 2/app.js
@@ -2,5 +2,14 @@ new Vue({
         el: '#exercise',
         data: {
             value: ''
+        },
+        methods: {
+            showAlert: function() {
+                console.log();
+                return alert('Clicked!')
+            },
+            storeVal: function(e) {
+                this.value = e.target.value
+            }
         }
     });
